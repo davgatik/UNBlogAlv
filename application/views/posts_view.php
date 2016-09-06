f30176fb7f66ecb8de35f32d3b6272aa77ab1132
davgatik <davgatik@gmail.com>
2016-09-06 11:03:36 -0500

    Sign In JQuery Validate

--- application/views/posts_view.php
+++ application/views/posts_view.php
@@ -42,7 +42,7 @@
           </ul>
         </li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class='glyphicon glyphicon-knight'></span>&nbsp;&nbsp;<?php if () echo  $this->session->userdata('usuario'); ?><span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class='glyphicon glyphicon-knight'></span>&nbsp;&nbsp;<?php echo  $this->session->userdata('usuario'); ?><span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="#">Action</a></li>
             <li><a href="#">Another action</a></li>
